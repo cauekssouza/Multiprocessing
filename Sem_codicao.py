890efca95ab1cd971eb2e724f6dc03846cd718ee
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2025-06-12 09:33:46 -0300

    Update Sem_codicao.py

--- Sem_codicao.py
+++ Sem_codicao.py
@@ -1,23 +1,24 @@
-from multiprocessing import Process, Value
+from multiprocessing import Process, Value, Lock
 import time
 import random
 
-def maquina(nome, estoque):
+def maquina(nome, estoque, lock):
     for _ in range(5):
-        atual = estoque.value
         time.sleep(random.uniform(0.1, 0.3))
-        estoque.value = atual + 1
-        print(f"{nome} produziu uma geladeira. Estoque atual é {estoque.value}")
+        with lock:  
+            estoque.value += 1
+            print(f"{nome} produziu uma geladeira. Estoque atual é {estoque.value}")
 
 if __name__ == '__main__':
-    estoque = Value('i', 0)  
+    estoque = Value('i', 0)
+    lock = Lock()  
     processos = []
     for i in range(3):
-        p = Process(target=maquina, args=(f'Máquina {i+1}', estoque))
+        p = Process(target=maquina, args=(f'Máquina {i+1}', estoque, lock))
         processos.append(p)
         p.start()
 
     for p in processos:
         p.join()
 
-    print(f"\n[Versão 1] Estoque final (sem controle de concorrência): {estoque.value}")
+    print(f"\n[Versão Com controle] Estoque final (sem condição de corrida): {estoque.value}")
